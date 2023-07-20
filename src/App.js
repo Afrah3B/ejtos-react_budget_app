1ccc6475249bda0fd1c324b11d6c3b90dce755a4
Afrah3B <135288691+Afrah3B@users.noreply.github.com>
2023-07-20 13:40:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,15 @@ const App = () => {
 					<div className='col-sm'>
 						<ExpenseTotal />
 					</div>
+                    <div className='col-sm'>
+                    <div className='alert alert-primary'>
+                    <select id="inputGroupSelect02" style={{color:'white', background: '#7EC3B5'}} /*onChange={(event) => setAction(event.target.value)}*/>
+						<option defaultValue value="$" name="Dollar">$ Dollar</option>
+	        	        <option value="£" name="Pound">£ Pound</option>
+                        <option value="€" name="Euro">€ Euro</option>
+	        	        <option value="₹" name="Ruppee">₹ Ruppee</option>
+				    </select>
+		            </div></div>
 				</div>
 				<h3 className='mt-3'>Allocation</h3>
 				<div className='row '>
